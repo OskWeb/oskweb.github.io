956bc11cf74042bc8bf5512ebdbcd44a2ad65c44
Oscar <oscararnu18@gmail.com>
2025-02-05 21:09:35 +0100

    deploy modified 4.0

--- dist/_astro/Logo.C-MmVS7y.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./jsx-runtime.D_zvdyIk.js";import"./index.BCtMShv3.js";const e=({url:a})=>s.jsxs("div",{className:"logoBox",children:[s.jsxs("a",{className:"logo",href:a,children:[s.jsx("span",{children:"Oscar"}),s.jsx("span",{children:"Aranda"})]}),s.jsx("div",{className:"divAnimated"})]});export{e as Logo};
