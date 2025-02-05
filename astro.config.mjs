aa770b738ddc752c53863b219f5716fd67201f64
Oscar <oscararnu18@gmail.com>
2025-02-06 00:19:55 +0100

    deploy version 9.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://oskweb.github.io",
   base: "oskweb.github.io",
   integrations: [react()],
   i18n: {
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,26 +3,17 @@ import { useState } from "react";
 
 export const Logo = ({ url }) => {
 
-    // const [isHovered, setIsHovered] = useState(false);
-
     return (
         <div className="logoBox">
 
             <a
                 className="logo"
-                href={url}
+                href={`/oskweb.github.io/${url}`}
             >
                 <span>Oscar</span>
                 <span>Aranda</span>
             </a>
             <div className="divAnimated"></div>
-
-            {/* <motion.div
-                initial={{ scale: 1 }}
-                animate={{ scale: isHovered ? 3 : 1 }}
-                transition={{ duration: 0.3 }}
-                className="divAnimated"
-            /> */}
         </div>
 
 
