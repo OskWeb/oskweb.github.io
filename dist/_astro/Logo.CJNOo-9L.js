85de3cdb8706b9d247f3a6f67a2ee570e632bddd
Oscar <oscararnu18@gmail.com>
2025-02-06 11:52:41 +0100

    deploy modified 17.0

---
+++ dist/_astro/Logo.CJNOo-9L.js
@@ -0,0 +1,1 @@
+import{j as s}from"./jsx-runtime.D_zvdyIk.js";import"./index.BCtMShv3.js";const e=({url:o})=>s.jsxs("div",{className:"logoBox",children:[s.jsxs("a",{className:"logo",href:`/oskweb.github.io/${o}`,children:[s.jsx("span",{children:"Oscar"}),s.jsx("span",{children:"Aranda"})]}),s.jsx("div",{className:"divAnimated"})]});export{e as Logo};
